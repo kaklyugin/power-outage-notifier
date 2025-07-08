f972e566912db67cd359657993a4faaf339607ec
a.kakliugin <a.kakliugin@1440.space>
2025-07-08 09:39:45 +0300

    Created modules

---
+++ tg-client/src/main/java/org/rostovenergoparser/tgclient/TgClientApplication.java
@@ -0,0 +1,13 @@
+package org.rostovenergoparser.tgclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgClientApplication.class, args);
+	}
+
+}
